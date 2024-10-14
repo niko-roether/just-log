8859a98c180fa07b4efb8f892cd268f6ae73d498
niko-roether <nicholas.roether@t-online.de>
2024-10-14 15:15:43 +0200

    Remove examples dist folder from version control

--- examples/node/dist/index.js
+++
@@ -1,9 +0,0 @@
-import log from "just-log";
-import initLog from "just-log-node";
-initLog();
-const logger = log.sub("example");
-logger.trace("trace message");
-logger.debug("debug message");
-logger.info("info message");
-logger.warning("warning message");
-logger.error("error message");
